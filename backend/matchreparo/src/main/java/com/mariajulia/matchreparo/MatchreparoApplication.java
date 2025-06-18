685905635824c4c901af7bda0c037a72742dc377
mariajuliasales <salesmariajulia11@gmail.com>
2025-06-18 16:34:24 -0300

    first commit

---
+++ backend/matchreparo/src/main/java/com/mariajulia/matchreparo/MatchreparoApplication.java
@@ -0,0 +1,13 @@
+package com.mariajulia.matchreparo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchreparoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchreparoApplication.class, args);
+	}
+
+}
---
+++ backend/matchreparo/src/test/java/com/mariajulia/matchreparo/MatchreparoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariajulia.matchreparo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchreparoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
