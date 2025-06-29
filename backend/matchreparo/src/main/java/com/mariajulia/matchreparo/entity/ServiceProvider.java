80ef03f790992527cf1ae254b89079e8b455acc6
mariajuliasales <salesmariajulia11@gmail.com>
2025-06-29 18:50:06 -0300

    feat: adicionar atributos a entidade Service Provider

--- backend/matchreparo/src/main/java/com/mariajulia/matchreparo/entity/ServiceProvider.java
+++ backend/matchreparo/src/main/java/com/mariajulia/matchreparo/entity/ServiceProvider.java
@@ -1,4 +1,20 @@
 package com.mariajulia.matchreparo.entity;
 
-public class ServiceProvider {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "service_provider")
+public class ServiceProvider extends User {
+
+    @Column(nullable = false, unique = true, length = 14)
+    private String cnpj;
+
 }
--- backend/matchreparo/src/main/java/com/mariajulia/matchreparo/repository/ServiceProviderRepository.java
+++ backend/matchreparo/src/main/java/com/mariajulia/matchreparo/repository/ServiceProviderRepository.java
@@ -1,4 +1,9 @@
 package com.mariajulia.matchreparo.repository;
 
-public interface ServiceProviderRepository {
+import com.mariajulia.matchreparo.entity.ServiceProvider;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ServiceProviderRepository extends JpaRepository<ServiceProvider, Long> {
 }
